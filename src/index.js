c93f1a298117d7ff1f71ee734d7f6785f9fd8a69
yining <yining.chen@viacom.com>
2017-07-25 19:07:34 -0400

    ...and remove phaser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*global Phaser*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,33 +6,3 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
-const config = {
-  width: 800,
-  height: 600,
-  renderer: Phaser.AUTO,
-  parent: '',
-  state: {
-    preload,
-    create,
-  },
-  transparent: false,
-  antialias: true,
-  physicsConfig: { arcade: true },
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.game.load.image('study', 'study.png');
-}
-
-function create() {
-  const { game } = this;
-  const objects = [
-    game.add.text(game.world.centerX, game.world.centerY * 0.8, `Welcome to Phaser`, { font: "bold 19px Arial", fill: "#fff" }),
-    game.add.sprite(game.world.centerX, game.world.centerY * 1.2, 'study')
-  ];
-
-  objects.forEach(obj => obj.anchor.setTo(0.5, 0.5));
-}
