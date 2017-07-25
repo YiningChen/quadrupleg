5c2f1bc72f31c0437091385cd3e5ce99df422b6b
yining <yining.chen@viacom.com>
2017-07-25 17:26:01 -0400

    bringing over phaser startert code...even though it breaks what we have currently

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
+/*global Phaser*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'pixi';
+import 'p2';
+import 'phaser';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// This is the entry point of your game.
+
+const config = {
+  width: 800,
+  height: 600,
+  renderer: Phaser.AUTO,
+  parent: '',
+  state: {
+    preload,
+    create,
+  },
+  transparent: false,
+  antialias: true,
+  physicsConfig: { arcade: true },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.game.load.image('study', 'study.png');
+}
+
+function create() {
+  const { game } = this;
+  const objects = [
+    game.add.text(game.world.centerX, game.world.centerY * 0.8, `Welcome to Phaser`, { font: "bold 19px Arial", fill: "#fff" }),
+    game.add.sprite(game.world.centerX, game.world.centerY * 1.2, 'study')
+  ];
+
+  objects.forEach(obj => obj.anchor.setTo(0.5, 0.5));
+}
