5dfabffd908037207264a3bc49b76ec71456c317
yining <yining.chen@viacom.com>
2017-07-25 18:18:08 -0400

    some phaser stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*global Phaser*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,3 +7,33 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const config = {
+  width: 800,
+  height: 600,
+  renderer: Phaser.AUTO,
+  parent: '',
+  state: {
+    preload,
+    create,
+  },
+  transparent: false,
+  antialias: true,
+  physicsConfig: { arcade: true },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.game.load.image('study', 'study.png');
+}
+
+function create() {
+  const { game } = this;
+  const objects = [
+    game.add.text(game.world.centerX, game.world.centerY * 0.8, `Welcome to Phaser`, { font: "bold 19px Arial", fill: "#fff" }),
+    game.add.sprite(game.world.centerX, game.world.centerY * 1.2, 'study')
+  ];
+
+  objects.forEach(obj => obj.anchor.setTo(0.5, 0.5));
+}
